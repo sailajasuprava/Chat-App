4b23c3ef2a8a413888c22b931e9df60913d721e0
sailajasuprava <sailaja.suprava@gmail.com>
2025-02-13 19:24:25 +0530

    socket connection

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,14 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import AuthProvider from "./context/authContext.jsx";
+import AuthProvider from "./context/AuthContext.jsx";
 import ConversationProvider from "./context/ConversationContext.jsx";
+import SocketProvider from "./context/SocketContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
       <ConversationProvider>
-        <App />
+        <SocketProvider>
+          <App />
+        </SocketProvider>
       </ConversationProvider>
     </AuthProvider>
   </StrictMode>
